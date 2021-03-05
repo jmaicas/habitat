51b7d2986dee3d272abf2153a2de93a854af1dda
jmaicas <jorgemaicas@gmail.com>
2021-03-05 22:24:48 +0000

    Time consistency check
    Detect times inconsistencies and add GUI elements to import from a single file or for several ones.

--- coincidences.py
+++ coincidences.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import platform
 import glob
 import re as re
 import numpy as np
@@ -29,7 +30,7 @@ sampling_time = 1/sampling_freq
 modules_dict = {"A1":32,"A2":33, "A3":34, "A4":35, "B1":36, "B2":37, "B3":38, "B4":39, \
                "C1":40,"C2":41, "C3":42, "C4":43, "D1":44,"D2":45, "D3":46, "D4":47, \
                 "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "11":11, "12":12, "13":13, \
-                "tC3":14, "tD3":15, "Norec":511}
+                "tC":14, "tD":15, "Norec":511}
 
 # d.items() >> d.keys() >> d.values() to use it and recover the keys.
 
@@ -42,12 +43,13 @@ class MyForm(QMainWindow):
     self.ui.setupUi(self)
 
     self.ui.ButtonSelectFolder.clicked.connect(self.selectFolder)
+    self.ui.ButDelFolder.clicked.connect(self.delFolder)
+    self.ui.ButtonResultFolder.clicked.connect(self.selectResultFolder)
+    self.ui.ButDelResultFolder.clicked.connect(self.delResultFolder)
+    self.ui.ButtonSelectFile.clicked.connect(self.selectFile)
     self.ui.ButDelFile.clicked.connect(self.delFile)
     self.ui.ButtonRunAll.clicked.connect(self.runAll)
-    self.ui.ButtonClearFigures.clicked.connect(self.closeFigures)
-    self.ui.Button2PDF.clicked.connect(self.print2pdf)
-    self.ui.Button2PNG.clicked.connect(self.print2png)
-
+    
     # Error message (it is necessary to initialize it too)
     self.error_msg = QErrorMessage()
     self.error_msg.setWindowTitle("Error")
@@ -57,49 +59,96 @@ class MyForm(QMainWindow):
   def selectFolder(self):
     DataFolder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
     if DataFolder:
-      self.ui.labelFileSelected.setText(DataFolder)
+      self.ui.labelFolderPath.setText(DataFolder)
+      self.delFile()
     else:
       self.error_msg.showMessage("It is necessary to select a folder")
 
+  def delFolder(self):
+    self.ui.labelFolderPath.clear()
+
+  def selectResultFolder(self):
+    self.ResultsFolder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+    if self.ResultsFolder:
+      self.ui.labelResultFolderPath.setText(self.ResultsFolder)
+    else:
+      self.error_msg.showMessage("It is necessary to select a folder")
+  
+  def delResultFolder(self):
+    self.ui.labelResultFolderPath.clear()
+  
+  def selectFile(self):
+    options = QFileDialog.Options()
+    self.filename, _ = QFileDialog.getOpenFileName(self,"Select Spreadsheet", "", "Excel files (*.xlsx)", options=options)
+    if self.filename:
+      self.ui.labelFilePath.setText(self.filename)
+      self.delFolder()
+  
   def delFile(self):
-    self.ui.labelFileSelected.clear()
+    self.ui.labelFilePath.clear()
 
   def runAll(self):
-    os.chdir(str(self.ui.labelFileSelected.text()))
-    d = os.getcwd() + "\\"
-    matching_files = glob.glob(r'*xlsx')
-    l_xlsx_files = []
-    self.rats = []
-    last_samples = []
+    if str(self.ui.labelResultFolderPath.text()) == '':
+      self.error_msg.showMessage("It is necessary to select an empty results folder")
+    else:
+      if str(self.ui.labelFolderPath.text()) != '' : 
+        # A folder path is given
+        os.chdir(str(self.ui.labelFolderPath.text()))
+      elif str(self.ui.labelFilePath.text()) != '' :
+        # A file path is given and the file contains several sheets
+        xls = pd.ExcelFile(self.filename)
+        os.chdir(str(self.ui.labelResultFolderPath.text()))
+        for n, sheet in enumerate(xls.sheet_names):
+          df = pd.DataFrame()
+          df = pd.read_excel(self.filename, sheet_name=sheet)
+          filename_out = 'rat_' + str(n+1) +'.xlsx' 
+          df.to_excel(filename_out, index = 0)
 
-    for matching_file in matching_files:
-      file_route = d + matching_file
-      l_xlsx_files.append(file_route)
+      
+      if platform.system() == 'Windows':
+        d = os.getcwd() + "\\" # windows 
+      else:
+        d = os.getcwd() + "/" # linux & MacOS
 
-      rat_n = rat()
+      matching_files = sorted(glob.glob(r'*xlsx')) # not sorted by name by default
+      l_xlsx_files = []
+      self.rats = []
+      self.rats_ids = []
+      last_samples = []
 
-      rat_n.read_excel(file_route, modules_dict)
-      rat_n.add_sampling(sampling_freq, sampling_time)
-      print(rat_n.rat_location)
-      print(rat_n.starting_time)
-      print(rat_n.rat_times)
-      self.rats.append(rat_n)
+      for matching_file in matching_files:
+        file_route = d + matching_file
+        l_xlsx_files.append(file_route)
 
-      last_samples.append(rat_n.last_sample)
+        rat_n = rat()
 
-    # for comparison, all the rats should have the same tracking time, the minimun
-    self.max_time = min(last_samples)
-    for rat_n in (self.rats):
-      rat_n.chop_times(self.max_time)
-      rat_n.basic_stats(modules_dict)
-      #print('modules or tunnels: ', rat_n.uniqueplaces)
-      #print('Modules names:', rat_n.unique_places_names)
-      #print('number of times: ', rat_n.uniquenumbertimes)
+        rat_n.read_excel(file_route, modules_dict)
+        if not rat_n.consistency:
+          error_message = "time errors in rat: " + str(rat_n.rat_id) + " columns: " + np.array2string(rat_n.excel_locations)
+          self.error_msg.showMessage(error_message)
+          
+        rat_n.add_sampling(sampling_freq, sampling_time)
+        #print(rat_n.rat_location)
+        #print(rat_n.starting_time)
+        #print(rat_n.rat_times)
+        self.rats.append(rat_n)
+        self.rats_ids.append(rat_n.rat_id)
+
+        last_samples.append(rat_n.last_sample)
+
+      # for comparison, all the rats should have the same tracking time, the minimun
+      self.max_time = min(last_samples)
+      for rat_n in (self.rats):
+        rat_n.chop_times(self.max_time)
+        rat_n.basic_stats(modules_dict)
+        #print('modules or tunnels: ', rat_n.uniqueplaces)
+        #print('Modules names:', rat_n.unique_places_names)
+        #print('number of times: ', rat_n.uniquenumbertimes)
 
 
-    #print ("Data and figures correctly exported to excel")
-    self.calc_tracking_coinc()
-    self.export_coincidences_to_excel()
+      #print ("Data and figures correctly exported to excel")
+      self.calc_tracking_coinc()
+      self.export_coincidences_to_excel()
 
   def export_coincidences_to_excel(self):
     workbook = xlsxwriter.Workbook('coincidencies.xlsx')
@@ -125,14 +174,22 @@ class MyForm(QMainWindow):
     workbook.close()
 
     # for clarity, another loop to export the coincidences per module per rat
+    writer = pd.ExcelWriter('coincident_time_per_module_per_rat.xlsx', engine='xlsxwriter')
     for rat_n in (self.rats):
-      the_other_rats = list(filter((rat_n).__ne__, self.rats)) # excludes the current rat from the list
+      #the_other_rats = list(filter((rat_n.rat_id).__ne__, self.rats_ids)) # excludes the current rat from the list
       list_modules = list(modules_dict.keys())
-      rat_df = pd.DataFrame(rat_n.shared_time_per_module_per_rat, 
-              columns=the_other_rats, index=list_modules)
-      
-      rat_df.to_excel('coincident_time_per_module_per_rat.xlsx', sheet_name=rat_n.rat_id)
+      rat_df = pd.DataFrame(rat_n.shared_time_per_module_per_rat, \
+                            columns=self.rats_ids, index=list_modules)
+      # adding averages per column (animals) and rows (modules&tunnels)
+      rat_df.loc['Totals',:] = rat_df.sum(axis=0) - rat_df.loc['Norec']
+      #cols_to_sum = rat_df.columns[ : rat_df.shape[1]]
+      #rat_df['Totals'] = rat_df[cols_to_sum].sum(axis=1) - rat_df[rat_n.rat_id]
 
+      #print(rat_df)
+      
+      rat_df.to_excel(writer, sheet_name=str(rat_n.rat_id))
+
+    writer.save()
 
   def closeFigures(self):
     plt.close('all')
--- coincidences_classes.py
+++ coincidences_classes.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.fromnumeric import nonzero
 import pandas as pd
 import xlsxwriter
 
@@ -10,19 +11,37 @@ class rat():
     rat_group = ""
     starting_time = 0
     sampling_f = 1
+    
 
   def read_excel(self, file_route, codes_dict):
     dfrat = pd.read_excel(file_route, sheet_name=0, dtype={'location':str}) # reads the first sheet
     self.rat_id = dfrat.iloc[0]['Subject']
+    self.consistency = True
     #self.rat_group = dfrat.iloc[0]['Description']
     self.dfnew = dfrat[["Behavior", "location", "Start (s)", "Stop (s)", "Duration (s)"]]
     # Changes modules names to numbers to make calculations faster
     self.dfnew["location"] = self.dfnew["location"].map(codes_dict)
     #print(self.dfnew)
+    self.check_consistency()
+  
+  def check_consistency(self):
+    previous_times = self.dfnew["Stop (s)"].to_numpy()
+    next_times = self.dfnew["Start (s)"].to_numpy()
+    margins = np.ones(len(next_times) -1 )*0.001
+    consistent_locations = (next_times[1:]- previous_times[:-1] - margins).round(3)
+    if sum(consistent_locations) != 0:
+      locations = np.nonzero(consistent_locations)
+      self.excel_locations = (locations + np.ones(len(locations))*3).astype(int)
+      self.consistency = False
+      
+      print("NON CONSISTENT TIMES DETECTED")
+      print("*****************************")
+      print("the non consistent locations are in rat: ", self.rat_id, ", excel columns:", self.excel_locations)
 
 
   def add_sampling(self, samp_freq, samp_time):
     self.sampling_f = samp_freq
+    self.sampling_time = 1/self.sampling_f
     self.starting_time = self.dfnew["Start (s)"][0]*samp_freq
     self.dfnew['samples'] = self.dfnew['Duration (s)']*samp_freq
     self.dfnew.round({'samples' : 0})
@@ -72,28 +91,48 @@ class rat():
 
   def calculate_coincidences(self, base_colum, location_matrix, modules_d):
     # first delete the colum of this rat
-    location_matrix = np.delete(location_matrix, base_colum, 1)
     this_rat_locations = np.transpose(self.rat_location)
     # matrix with zeros where there is a coincidence
-    substracted_matrix = location_matrix - this_rat_locations[:,None]
+    substracted_matrix_all = location_matrix - this_rat_locations[:,None]
+    truth_matrix_all = (substracted_matrix_all == 0)
+    
+    # Compares one rat at a time with each of the other rats to get the 
+    # total amount of time shared with each rat in each module or tunnel
+    # we already have the matrix with all the differences (truth_matrix)
+    # we are going to export a matrix with the modules as rows and the other rats as columns
+    shared_time_per_module_per_rat = np.zeros((len(modules_d), truth_matrix_all.shape[1]))
+    # loop through every module
+    for n, (mod_name, mod_value) in enumerate(modules_d.items()):
+      # loope through every rat
+      for rat_column in np.arange(truth_matrix_all.shape[1]):
+        mod_truth_rat = truth_matrix_all[this_rat_locations == mod_value, rat_column]
+        shared_time_per_module_per_rat[n][rat_column] = np.sum(mod_truth_rat)
+    
+    self.shared_time_per_module_per_rat = shared_time_per_module_per_rat*self.sampling_time
+
+    # first delete the colum of this rat
+    location_matrix_minus_one_rat = np.delete(location_matrix, base_colum, 1)
+    # matrix with zeros where there is a coincidence
+    substracted_matrix = location_matrix_minus_one_rat - this_rat_locations[:,None]
     truth_matrix = (substracted_matrix == 0)
 
     # Overall coincidences. Number of times with 1 or more other animals.
-    sum_truth_matrix = np.sum(truth_matrix, axis = 1)
-    sum_matrix_downsampled = sum_truth_matrix[::1000].copy()
-    rat_locations_downsampled = self.rat_location[::1000].copy()
-    
-    sort_idx = np.argsort(list(modules_d.values()))
-    idx = np.searchsorted(list(modules_d.values()),rat_locations_downsampled,sorter = sort_idx)
-    out = np.asarray(list(modules_d.keys()))[sort_idx][idx]
-    
-    dfcoinc = pd.DataFrame()
-    dfcoinc['location'] = out
-    dfcoinc['coinc n#'] = sum_matrix_downsampled
-    filename = 'coincidencesbysecond_rat' + str(self.rat_id) + '.xlsx'
-    dfcoinc.to_excel(filename)
-    unique, counts = np.unique(sum_truth_matrix, return_counts=True)
-    self.companions_times = dict(zip(unique, counts))
+    # Export stats per second (should be optional, it takes time)
+    #sum_truth_matrix = np.sum(truth_matrix, axis = 1)
+    #sum_matrix_downsampled = sum_truth_matrix[::1000].copy()
+    #rat_locations_downsampled = self.rat_location[::1000].copy()
+    #
+    #sort_idx = np.argsort(list(modules_d.values()))
+    #idx = np.searchsorted(list(modules_d.values()),rat_locations_downsampled,sorter = sort_idx)
+    #out = np.asarray(list(modules_d.keys()))[sort_idx][idx]
+    #
+    #dfcoinc = pd.DataFrame()
+    #dfcoinc['location'] = out
+    #dfcoinc['coinc n#'] = sum_matrix_downsampled
+    #filename = 'coincidencesbysecond_rat' + str(self.rat_id) + '.xlsx'
+    #dfcoinc.to_excel(filename)
+    #unique, counts = np.unique(sum_truth_matrix, return_counts=True)
+    #self.companions_times = dict(zip(unique, counts))
 
     # Per module or tunnel
     self.l_companions_times_per_module= []
@@ -101,28 +140,8 @@ class rat():
       mod_truth_matrix = truth_matrix[this_rat_locations == mod_value, :]
       sum_truth_matrix_mod = np.sum(mod_truth_matrix, axis = 1)
       unique, counts = np.unique(sum_truth_matrix_mod, return_counts=True)
-      companions_times = dict(zip(unique, counts/self.sampling_f))
+      companions_times = dict(zip(unique, counts*self.sampling_time))
       self.l_companions_times_per_module.append((mod_name, companions_times))
 
     #print("Id: ", self.rat_id, ' Number of companions per module: ', self.l_companions_times_per_module)
-
-    # now compares one rat at a time with each of the other rats to get the 
-    # total amount of time shared with each rat in each module or tunnel
-    # we already have the matrix with all the differences (truth_matrix)
-    # we are going to export a matrix with the modules as rows and the other rats as columns
-    self.shared_time_per_module_per_rat = np.zeros((len(modules_d), truth_matrix.shape[1]))
-    # loop through every module
-    for n, (mod_name, mod_value) in enumerate(modules_d.items()):
-      # loope through every rat
-      for rat_column in truth_matrix.shape[1]:
-        mod_truth_rat = truth_matrix[this_rat_locations == mod_value, rat_column]
-        self.shared_time_per_module_per_rat[n][rat_column] = np.sum(mod_truth_rat)
-
-
-
-
-# No estoy seguro de querer la matriz de non zeros. La matriz de nonzeros sirve para saber el tiempo total que
-# el animal ha pasado en compania. Ni siquiera, porque hay muchas columnas.
-#
-#  Haz algo simple con la matriz de coincidencias. Usar la booleana para sumar zeros en cada columna.
-# Me va a faltar el nombre de la rata.
+    
